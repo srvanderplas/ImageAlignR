53caa6dfc72f0778a9bf7a09b9d23888267dead4
Susan Vanderplas <srvanderplas@gmail.com>
2018-09-11 20:30:57 -0500

    Fixing small issues

--- R/rotate_and_refit.R
+++ R/rotate_and_refit.R
@@ -2,34 +2,45 @@
 #'
 #' This should ensure that the major axis of the ellipse is vertical.
 #' @param img a cimg of the object to be rotated
-#' @param ellipse the original (potentially tilted) ellipse
 #' @param ... additional parameters to pass to imrotate
 #' @importFrom assertthat assert_that
+#' @importFrom imager pad bucketfill rotate_xy autocrop
+#' @importFrom stats spectrum
 #' @export
-img_rotate_refit <- function(img, ellipse, ...) {
+img_rotate_refit <- function(img, ...) {
 
   # Clean up inputs and check
   img <- img_check(img, keep_alpha = T, keep_color = T)
+  
+  ellipse <- img %>%
+    outer_contour() %>%
+    thin_contour(img = img) %>%
+    contour_ellipse_fit()
+  
   ellipse_check(ellipse)
 
+  theta <- -ellipse$Angle
+  if (theta > 45) theta <- theta - 90
+  if (theta < (-45)) theta <- theta + 90
+  
   n <- 250
   imgrot <-
     img %>%
-    imager::pad(n, "xy", pos = 1, val = rep(1, spectrum(.))) %>%
-    imager::pad(n, "xy", pos = -1, val = rep(1, spectrum(.))) %>%
+    imager::pad(n, "xy", pos = 1, val = rep(1, imager::spectrum(.))) %>%
+    imager::pad(n, "xy", pos = -1, val = rep(1, imager::spectrum(.))) %>%
     imager::bucketfill(1, 1, color = c(1, 1, 1), sigma = .1) %>% 
-    imager::rotate_xy(angle = -ellipse$Angle, cx = ellipse$CenterX + n,
+    imager::rotate_xy(angle = theta, cx = ellipse$CenterX + n,
                       cy = ellipse$CenterY + n,
                       interpolation = 1, boundary = 2) %>%
-    autocrop
+    imager::autocrop()
 
   refit <- imgrot %>%
     outer_contour() %>%
     thin_contour(img = imgrot) %>%
     contour_ellipse_fit()
   
-  plot(imgrot)
-  ellipse_points(rot_ellipse, col = "orange")
+  graphics::plot(imgrot)
+  ellipse_points(refit, col = "orange")
   
   list(img = imgrot, ellipse = refit)
 }
